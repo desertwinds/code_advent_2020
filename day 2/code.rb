bb211a432f5c96016d5e21d5e13328a8d55c2a08
Javier Antonini <javier@bridge-u.com>
2020-12-03 17:54:59 +0100

    Added part 2 of day 2

--- day 2/code.rb
+++ day 2/code.rb
@@ -1,22 +1,48 @@
 valid_passwords = []
 invalid_passwords = []
 
+puts "Enter option 1 or 2 for the puzzle solution"
+type = gets.chomp.to_i
+if type < 1 || type > 2
+  puts "Sorry, can't handle that type, only allowed value is 1 or 2"
+  return
+end
+
 File.foreach("input.txt") do |password_line|
   /(?<lower>\d+)-(?<highest>\d+) (?<constraint>.): (?<password>\w+)/ =~
     password_line
-  repetitions = password.count(constraint)
-  if repetitions < lower.to_i || repetitions > highest.to_i
-    invalid_passwords.push({
-      password: password,
-      constraint: constraint,
-      limits: "#{lower}-#{highest}"
-    })
+
+  password_object = {
+    password: password,
+    constraint: constraint,
+    limits: "#{lower}-#{highest}"
+  }
+  if type == 1
+    repetitions = password.count(constraint)
+    if repetitions < lower.to_i || repetitions > highest.to_i
+      invalid_passwords.push(password_object)
+    else
+      valid_passwords.push(password_object)
+    end
   else
-    valid_passwords.push({
-      password: password,
-      constraint: constraint,
-      limits: "#{lower}-#{highest}"
-    })
+    if lower.to_i > password.length || highest.to_i > password.length
+      invalid_passwords.push(password_object)
+      puts "Length missmatch"
+      next
+    end
+    if password[lower.to_i - 1] == constraint
+      if password[highest.to_i - 1] == constraint
+        invalid_passwords.push(password_object)
+      else
+        valid_passwords.push(password_object)
+      end
+    else
+      if password[highest.to_i - 1] == constraint
+        valid_passwords.push(password_object)
+      else
+        invalid_passwords.push(password_object)
+      end
+    end
   end
 end
 
